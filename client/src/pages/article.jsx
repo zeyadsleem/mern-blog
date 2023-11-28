078f04890f19ccaf489bd751c334ecc6c2bf2a1b
zeyadsleem <zeyadsleem1@gmail.com>
2023-11-28 20:44:05 +0200

    format: server

--- client/src/pages/article.jsx
+++ client/src/pages/article.jsx
@@ -1,34 +1,21 @@
-import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import articles from "../content.js";
-import NotFound from "../pages/404.jsx";
+import NotFound from "./404.jsx";
 
 export default function Articale() {
   const { articleId } = useParams();
-  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+  const article = articles.find(article => article.name === articleId)
 
-  useEffect(() => {
-    const fetchArticleInfo = async () => {
-      const result = await fetch(`/api/articles/${name}`);
-      const body = await result.json();
-      setArticleInfo(body);
-    };
-
-    fetchArticleInfo();
-  }, []);
-
-  const article = articles.find((article) => article.name === articleId);
   if (!article) {
-    return <NotFound />;
+    return <NotFound />
   }
 
   return (
     <>
       <h1>{article.title}</h1>
-      <p>this article has {articleInfo.upvotes} upvotes</p>
       {article.content.map((paragraph) => (
         <p key={paragraph}>{paragraph}</p>
       ))}
     </>
-  );
+  )
 }
