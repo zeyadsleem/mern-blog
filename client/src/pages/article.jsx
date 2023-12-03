8f3b19f6f57ada60950377a9c966a91e27d15dc2
zeyadsleem <zeyadsleem1@gmail.com>
2023-12-03 15:35:13 +0200

    chor: add useEffect to articale list

--- client/src/pages/article.jsx
+++ client/src/pages/article.jsx
@@ -1,10 +1,17 @@
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import articles from '../content.js'
 import NotFound from './404.jsx'
 
 export default function Articale() {
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] })
+
     const { articleId } = useParams()
     const article = articles.find((article) => article.name === articleId)
+
+    useEffect(() => {
+        setArticleInfo({ upvotes: Math.ceil(Math.random() * 10), comments: ['comment1', 'comment2'] })
+    }, [])
 
     if (!article) {
         return <NotFound />
@@ -13,6 +20,7 @@ export default function Articale() {
     return (
         <>
             <h1>{article.title}</h1>
+            <p>This article has {articleInfo.upvotes} upvotes</p>
             {article.content.map((paragraph) => (
                 <p key={paragraph}>{paragraph}</p>
             ))}
