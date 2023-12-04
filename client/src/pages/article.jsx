34406d80383cdb783179ff922bc488a1e55b7a4f
zeyadsleem <zeyadsleem1@gmail.com>
2023-12-04 15:40:18 +0200

    chor: use in memory  DB works fin

--- client/src/pages/article.jsx
+++ client/src/pages/article.jsx
@@ -1,29 +1,29 @@
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import articles from '../content.js'
+// import { useParams } from 'react-router-dom'
+// import articles from '../content.js'
 import NotFound from './404.jsx'
 
 export default function Articale() {
-    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] })
+    // const { articleId } = useParams()
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 10, comments: ["test"] });
 
-    const { articleId } = useParams()
-    const article = articles.find((article) => article.name === articleId)
+    // useEffect(() => {
+    //     const fetchArticleInfo = async () => {
+    //         const result = await fetch(`/api/articles/${articleId}`);
+    //         const body = await result.json();
+    //         setArticleInfo(body);
+    //     };
+    //     fetchArticleInfo();
+    // }, [articleId]);
 
-    useEffect(() => {
-        setArticleInfo({ upvotes: Math.ceil(Math.random() * 10), comments: ['comment1', 'comment2'] })
-    }, [])
-
-    if (!article) {
+    if (!articleInfo) {
         return <NotFound />
     }
 
     return (
         <>
-            <h1>{article.title}</h1>
-            <p>This article has {articleInfo.upvotes} upvotes</p>
-            {article.content.map((paragraph) => (
-                <p key={paragraph}>{paragraph}</p>
-            ))}
+            <p>{articleInfo.upvotes}</p>
+            <p>{articleInfo.comments}</p>
         </>
     )
 }
